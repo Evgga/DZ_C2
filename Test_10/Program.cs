673641b917ea42c00b19ce47cff9305a318172ce
Evgenia <evgga@mail.ru>
2023-02-17 20:28:26 +0900

    Задача 10

---
+++ Test_10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10. Напишите программу,ктр принимает на вход 3хзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Вторая цифра этого числа -> "+stringNumber[1]);
+
+
